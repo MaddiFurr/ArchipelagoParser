20e99c3a97d259b90b5e215d47e75eb02f8ace4e
Maddi <16472317+MaddiFurr@users.noreply.github.com>
2023-09-14 09:03:19 -0500

    testing changes

--- parse.py
+++ parse.py
@@ -2,6 +2,7 @@ import re
 import json
 import sys
 import time
+import os
 
 print("Welcome to Archipeago Spoiler Parser\nParsing File: " + sys.argv[1])
 time.sleep(3)
@@ -12,10 +13,6 @@ players = []
 spherelines = []
 line_count = 0
 
-class Player:
-    spheres =[]
-    highest = None
-
 def playerlist(): #Get the list of all the players in the game
     regx = re.compile ('^([^:]+): *(.*?) *$',re.MULTILINE)
     with open(sys.argv[1], 'r') as f:
@@ -25,7 +22,6 @@ def playerlist(): #Get the list of all the players in the game
     for line in lines:
         dictionary.update(regx.findall(line))
 
-    print("\n\nGame Seed: " + dictionary["Archipelago Version 0.4.2  -  Seed"] + "\n")
 
     playercount = int(dictionary["Players"])
     players = []
@@ -37,70 +33,110 @@ def playerlist(): #Get the list of all the players in the game
     dictionary.clear()
     return players
 
-def sphere_count(): #Get the total number of spheres
+def sphere_count(pts): #Get the total number of spheres
     line_count = 0
     with open(sys.argv[1], 'r') as f:
         for ln in f:
             line_count += 1
-            if ': {' in ln:
+            if ': {' in ln and line_count > pts:
                 spherelines.append(line_count)
     return line_count, spherelines
 
 
-def player_spheres(p,spherelines):
+def player_spheres(p,spherelns,pts):
     line_count = 0
     lines = []
     spheres = []
-    highestsphere = 0
+    highestline = 0
     cursphere = 1
     with open(sys.argv[1], 'r') as f:
         for ln in f:
             line_count += 1
-            search = ln.split(":")
-            if p in search[0] and line_count > spherelines[0]:
+            search = ln.split("):")
+            
+            if p in search[0] and line_count > spherelns[0] and line_count > pts:
                 lines.append(line_count)
-                print("Line Number: " + str(line_count))
+                highestline = line_count
             
     f.close()
-
-    i = 0
-    for i in range(len(lines)):
-        if lines[i] > spherelines[cursphere]:
-            i += 1
-            highestsphere = cursphere
-            cursphere += 1
-            if cursphere >= len(spherelines):
-                break
-            if i > len(lines):
-                break
-        
+    s = 0
+    print(lines)
+    allspheres = []
+    highestsphere = s
+    print(spherelns)
+    chk = True
+    while chk:
+        if highestline > spherelns[s]:
+            s += 1
             
-    print("Highest Sphere: " + str(highestsphere))
-
-
+            if s == len(spherelns):
+                highestsphere = len(spherelns)
+                chk = False
+        else:
+            highestsphere = s
+            chk = False
+    
+    print(p + " Highest Sphere: " + str(highestsphere))
+    
+    highestsphere -= 1
+    return highestsphere
                 
-                    
-                        
-    pass
-## the idea here is to find out where each of the phases are eg '1: {'. Iterate through all numbers until there isn't a line with the text of a number
-## then add all of those line numbers to a list. then go down each player and find an occurance of their name. Check the line against the list of lines
-## for each number and then find out which one is the highest and create the complettion list based on that.
+def find_the_fucking_playthrough():
+    line_count = 0
+    with open(sys.argv[1], 'r') as f:
+        for ln in f:
+            line_count += 1
+            if 'Playthrough:' in ln:
+                return line_count
+    
+def prune():
+    lowline = 0
+    highline = 0
+    with open(sys.argv[1], 'r') as f:
+        lines = f.readlines()
+        for ln in f:
+            line_count += 1
+            if 'Entrances:' in ln and line_count > pts:
+                lowline = line_count
+            if 'Playthrough:' in ln and line_count > pts:
+                highline = line_count -1
+        f.truncate()
+        for number, line in enumerate(lines):
+            if number not in [lowline, highline]:
+                f.write(line)
 
-line_count, spherelines = sphere_count()
+prune()
+
+pts = find_the_fucking_playthrough()
+lncnt, spherelines = sphere_count(pts)
 players = playerlist()
 
-
-print("line Count: " + str(line_count))
-print('Total Spheres: ' + str(len(spherelines)))
-
-print("\nPlayer List:")
-i = 0
-for i in range(len(players)):
-    print(players[i])
-
+if os.path.isfile(".lines"):
+    os.remove(".lines")
 
 i = 0
 for i in range(len(players)):
-    print("\n\nFinding Sphere occurrences for player: " + players[i])
-    
-    player_spheres(players[i],spherelines)
+    if players[i] != "Spectator":
+        sphere = player_spheres(players[i],spherelines,pts)
+        temp = open(".lines", "a")
+        temp.write(players[i] + "||" + str(sphere) + "\n")
+        temp.close()
+        i += 1
+
+
+ufinish = []
+with open(".lines") as f:
+    for line in f:
+        name, sphere = line.strip().split("||")
+        
+        ufinish.append((name, int(sphere)))
+
+        finishorder = sorted(ufinish, key=lambda x: x[1], reverse=False)
+
+    print('\n\nPredicted Archipelago Finish Order\n<Completion Position>: ("YMAL NAME", <Finishing Sphere>)\n')
+    i = 1
+    for item in finishorder:
+        print("Position " + str(i) + ": " + str(item))
+        i += 1
+f.close()
+os.remove(".lines")
